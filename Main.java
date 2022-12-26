63366ebf570bef2712f7cef68d45669a16cfc759
FAIZAN <115339180+mdfaizanofficial@users.noreply.github.com>
2022-12-26 18:32:45 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.faizan;
+
+public class Main {
+  public static void main(String[] args) {
+    Account faizan = new Account("919837240430", 0.0, "Faizan", "alamfaizan173@gmail.com", "9837240430");
+    faizan.DepositMoney(100);
+    faizan.WithDrawMoney(200);
+
+  }
+}
